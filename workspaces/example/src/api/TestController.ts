61991611135c0866a943f915fc791c8b62a47ad3
Bendeguz Cseresnyes <cser.bende@gmail.com>
2022-08-16 23:22:54 +0200

    Update example

--- workspaces/example/src/api/TestController.ts
+++ workspaces/example/src/api/TestController.ts
@@ -2,14 +2,7 @@ import { Route } from "citrine";
 
 @Route.Prefix("/test")
 export default class TestController {
-  constructor() {
-    Route.AddRoute(this, "get", "/test", () => this.index());
-    Route.SetConfig(this, "prefix", "/test2");
-    console.log(Route.GetRoutes(this));
-    console.log(Route.GetConfig(this));
-  }
-
-  @Route.Get()
+  @Route.Get("/")
   async index() {
     console.log("first");
     return "HelloWorld";
