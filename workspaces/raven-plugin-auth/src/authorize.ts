7dc4820e1d8a3e2f3b751873b76404bf14fcca62
Bendeguz Cseresnyes <cser.bende@gmail.com>
2022-08-28 08:47:03 +0200

    disable validation annotation to controllers

--- workspaces/raven-plugin-auth/src/authorize.ts
+++ workspaces/raven-plugin-auth/src/authorize.ts
@@ -3,8 +3,8 @@ import RavenPluginAuth from '.'
 
 export const AuthorizeSymbol = Symbol('Authorize')
 
-export function Authorize(authorize: boolean = true): ClassDecorator & MethodDecorator {
-  return (target: Object | Function, propertyKey?: string | symbol) => {
+export function Authorize(authorize: boolean = true): MethodDecorator {
+  return (target: Object | Function, propertyKey: string | symbol) => {
     Validate.PluginInitialized(RavenPluginAuth)(target, propertyKey)
     Route.Annotate(AuthorizeSymbol, authorize)(target, propertyKey)
   }
--- workspaces/raven/src/validate/createAnnotation.ts
+++ workspaces/raven/src/validate/createAnnotation.ts
@@ -6,7 +6,7 @@ export type ValidatorFn = (ctx: Context, raven: Raven) => void | Promise<void>
 
 export const ValidationSymbol = Symbol()
 
-export function createAnnotation(validator: ValidatorFn) {
+export function createAnnotation(validator: ValidatorFn): (target: Object, propertyKey: string | symbol) => void {
   return Route.Annotate(ValidationSymbol, (validators?: ValidatorFn[]) => {
     return Array.isArray(validators) ? [...validators, validator] : [validator]
   })
